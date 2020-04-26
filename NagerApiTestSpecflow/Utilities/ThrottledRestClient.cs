82e6f8cef7108e48c48e8b5dd0f9b331dbb85369
nekoyama <orozco.hildam@gmail.com>
2020-04-26 15:10:29 +0200

    Clean repository

---
+++ NagerApiTestSpecflow/Utilities/ThrottledRestClient.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using RestSharp;
+
+namespace NagerResponse.Utilities
+{
+    public class ThrottledRestClient : RestClient
+    {
+        private readonly int _requestsPerMinute;
+        private int _lastRequestTime;
+
+        public ThrottledRestClient(int requestsPerMinute)
+        {
+            _requestsPerMinute = requestsPerMinute;
+        }
+        public override IRestResponse Execute(IRestRequest request)
+        {
+            int elapsedTime = Environment.TickCount - _lastRequestTime;
+            int pause = (60 / _requestsPerMinute) * 1000;
+            int wait = pause - elapsedTime;
+            if (wait > 0)
+            {
+                Thread.Sleep(wait);
+            }
+            var response = base.Execute(request);
+            _lastRequestTime = Environment.TickCount;
+            return response;
+        }
+    }
+}
